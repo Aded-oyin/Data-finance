557958a0446081f38beca1e1afd629644f8ee5ae
Aded-oyin <danyodele50@gmail.com>
2023-11-14 13:32:49 +0100

    Completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Typed from 'react-typed'
+
 
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
-                <Typed className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2' strings={['BTB', 'BTC', 'SASS']} typeSpeed={120} backspeed={140} loop />
+                <span className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'>BTB</span>
             </div>
             <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to incease revenue for BTB, BTC, & SASS platforms.</p>
             <button
